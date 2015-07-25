6a1dcd2ff3173453a5fd6c8a5527dca9b0dabca0
Rikard Eide <rix1@Rikards-MacBook-Pro.local>
2015-07-25 12:56:00 +0200

    landing page ready for demo

--- app/home/home.js
+++ app/home/home.js
@@ -2,13 +2,24 @@
 
 angular.module('myApp.home', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/home', {
+            templateUrl: 'home/home.html',
+            controller: 'HomeCtrl'
+        });
+    }])
 
-.controller('HomeCtrl', [function() {
+    .controller('HomeCtrl', function FirstCtrl($scope) {
+        var c = this;
 
-}]);
+        c.topics = [{"name":"Pitching","icon":"fa fa-comment","desc":"Selling your idea","selected":false},{"name":"Growth Hacking","icon":"fa fa-line-chart","desc":"Marketing on a budget","selected":false},{"name":"Business Models","icon":"fa fa-briefcase","desc":"Where does the money come from?","selected":false},{"name":"Building Team","icon":"fa fa-users","desc":"You're only as strong as your weakest link","selected":false},{"name":"Finding Ideas","icon":"fa fa-lightbulb-o","desc":"They don't grow on trees","selected":false},{"name":"Developing","icon":"fa fa-code","desc":"Everyone should code","selected":false}];
+
+        $scope.changeClass = function (topic) {
+            $scope.selected = true;
+            topic.selected = !topic.selected;
+            //$scope.box = "selected";
+            console.log(topic);
+        };
+
+
+    });
