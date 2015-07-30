12e2da8b0a9cef7f38472833252436a6827b7e28
rix1 <rikardeide@gmail.com>
2015-07-30 09:34:58 +0200

    Fixed yanked footer

--- app/views/landing/landing.js
+++ app/views/landing/landing.js
@@ -16,5 +16,5 @@ angular.module('myApp.landing', ['ngRoute'])
             $scope.selected = true;
         };
 
-        c.topics = [{"name":"Pitching","icon":"fa fa-comment","desc":"Selling your idea","selected":false},{"name":"Growth Hacking","icon":"fa fa-line-chart","desc":"Marketing on a budget","selected":false},{"name":"Business Models","icon":"fa fa-briefcase","desc":"Where does the money come from?","selected":false},{"name":"Building Team","icon":"fa fa-users","desc":"You're only as strong as...","selected":false},{"name":"Finding Ideas","icon":"fa fa-lightbulb-o","desc":"They don't grow on trees","selected":false},{"name":"Developing","icon":"fa fa-code","desc":"Everyone should code","selected":false}];
+        c.topics = [{"title":"Pitching","icon_url":"fa fa-comment","desc":"Selling your idea","selected":false},{"title":"Growth Hacking","icon_url":"fa fa-line-chart","desc":"Marketing on a budget","selected":false},{"title":"Business Models","icon_url":"fa fa-briefcase","desc":"Where does the money come from?","selected":false},{"title":"Building Team","icon_url":"fa fa-users","desc":"You're only as strong as...","selected":false},{"title":"Finding Ideas","icon_url":"fa fa-lightbulb-o","desc":"They don't grow on trees","selected":false},{"title":"Developing","icon_url":"fa fa-code","desc":"Everyone should code","selected":false}];
     }]);
