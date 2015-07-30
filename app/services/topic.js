5a82696a8c92e34b53e20e2084b9b4e15dafd138
andrroy <andreas@royrvik.org>
2015-07-30 13:39:08 +0200

    Added simple Topic service

---
+++ app/services/topic.js
@@ -0,0 +1,39 @@
+angular.module('corsaneApp').factory('topicService', ['$http', 'config', '$q',
+
+    function($http, config, $q) {
+
+        return {
+
+            getTopic: function(id) {
+
+                var defer = $q.defer();
+
+                $http.get(config.baseUrl + 'topic/' + id)
+                    .success(function(res) {
+                        defer.resolve(res);
+                    })
+                    .error(function(err, status) {
+                        defer.reject(err)
+                    });
+
+                return defer.promise;
+            },
+
+            getAllTopics: function() {
+
+                var defer = $q.defer();
+
+                $http.get(config.baseUrl + 'topic/')
+                    .success(function(res) {
+                        defer.resolve(res);
+                    })
+                    .error(function(err, status) {
+                        defer.reject(err)
+                    });
+
+                return defer.promise;
+            }
+
+        }
+    }
+]);
