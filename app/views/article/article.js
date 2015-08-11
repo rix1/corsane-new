9232444db6b1a42e92f9112bbb6d2b09ca102a1d
rix1 <rikardeide@gmail.com>
2015-08-11 13:21:44 +0200

    basic layout in article editor now in place. updated linking in profile and lab

--- app/views/article/article.js
+++ app/views/article/article.js
@@ -30,8 +30,57 @@ angular.module('myApp.article', ['ngRoute'])
 
 
     .controller('newArticleCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+        var first = true;
+        $scope.signedIn = false;
+
+        $scope.helptext = '';
+
+        $scope.article = {
+            title: '',
+            introduction: '',
+            topics: '',
+            published: false,
+            featured: false,
+            approved: false
+        };
+
+        var paragraph = {
+            article: '',
+            headline: '',
+            text: ''
+        };
+
+        $scope.$watch(function ($scope) {
+            return $scope.article.title;
+        }, function (newval, oldval) {
+            console.log(newval);
+            console.log(oldval);
+        });
+
+        $scope.content = [];
+        console.log($rootScope.user);
+
+
         if($rootScope.user){
             $scope.signedIn = true;
+            $scope.article.author = $rootScope.user.id;
         }
-        console.log("create new article naow");
+
+        $scope.addParagraph = function (test) {
+            console.log(test);
+            var a = $scope.article;
+
+            //console.log($scope.article);
+            if(!$scope.signedIn) {
+                $scope.helptext = 'Please sign in to create a new article';
+            }else if((a.title && a.introduction && a.topics)){
+                $scope.helptext = 'Please fill inn Title, Description and Topic before continuing...';
+            }else{
+                if(first){
+                    // TODO: Add article to API and set articleID
+                    first = false;
+                }
+                $scope.content.push(paragraph);
+            }
+        };
     }]);
