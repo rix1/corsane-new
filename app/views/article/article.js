5fc9d0e2bf22f0de9f4310b1e368bb519107fa1e
rix1 <rikardeide@gmail.com>
2015-08-11 10:43:17 +0200

    Added editable header fields to newArticle view. Committing before bower update.

--- app/views/article/article.js
+++ app/views/article/article.js
@@ -2,22 +2,36 @@
 
 angular.module('myApp.article', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/article/:id', {
             templateUrl: 'views/article/article.html',
             controller: 'articleCtrl'
+        }).when('/new/article/', {
+            templateUrl: 'views/article/newArticle.html',
+            controller: 'newArticleCtrl'
         });
     }])
 
-    .controller('articleCtrl', ['$scope', '$routeParams', 'articleService', function($scope, $routeParams, articleService) {
+    .controller('articleCtrl', ['$scope', '$routeParams', 'articleService', function ($scope, $routeParams, articleService) {
         var articleId = $routeParams.id;
+
+        console.log(articleId);
+
         articleService.getArticle(articleId).then(
-            function(article) {
+            function (article) {
                 console.log(article);
                 $scope.article = article;
             },
-            function(err) {
+            function (err) {
                 console.log(err);
             }
         );
+    }])
+
+
+    .controller('newArticleCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+        if($rootScope.user){
+            $scope.signedIn = true;
+        }
+        console.log("create new article naow");
     }]);
