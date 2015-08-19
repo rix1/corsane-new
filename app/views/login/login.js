cf2674da790aa073d057dbcd8b0f1a2c64e01436
andrroy <andreas@royrvik.org>
2015-08-19 14:06:03 +0200

    Started working on separating login and register into two views. Login-view now works

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -16,8 +16,43 @@ angular.module('myApp.login', ['ngRoute'])
             })
     }])
 
-    .controller('authCtrl', ['$scope', function($scope) {
-        // something
+    .controller('authCtrl', ['$scope', '$rootScope', '$location', 'authService',
+        function($scope, $rootScope, $location, authService) {
+
+        $scope.pending = false;
+        $scope.error = {err:false, msg:""};
+
+        // Redirect logged in users to their profile
+        if($rootScope.user) {
+            $location.path("/profile");
+        }
+
+        $scope.submit = function (form) {
+            $scope.error.err = false;
+
+            if (!form || !form.username || !form.password) {
+                $scope.error.err = true;
+                $scope.error.msg = "Please fill in the fields";
+            } else {
+                $scope.pending = true;
+                login(form);
+            }
+        };
+
+        var login = function (credentials) {
+            authService.login(credentials)
+                .then(function (res) {
+                    $location.path("/profile");
+                }, function (err) {
+                    $scope.pending = false;
+                    $scope.error.msg = err.message;
+                    $scope.error.err = true;
+                });
+        };
+
+        $scope.loginWithFacebook = function() {
+            authService.loginWithFacebook();
+        };
     }])
 
 
@@ -30,7 +65,9 @@ angular.module('myApp.login', ['ngRoute'])
         })
     }])
 
-    .controller('logoutCtrl', ['$rootScope', '$scope', '$location', 'authService', function($rootScope, $scope, $location, authService) {
+    .controller('logoutCtrl', ['$rootScope', '$scope', '$location', 'authService',
+        function($rootScope, $scope, $location, authService) {
+
         authService.logout()
             .then(function (res) {
                 delete $rootScope.user;
