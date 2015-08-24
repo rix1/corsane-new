99bbd8c29afba45136d5ef67177f6fb49672a100
andrroy <andreas@royrvik.org>
2015-08-24 20:57:06 +0200

    Added some styling to change password

--- app/views/profile/profile.js
+++ app/views/profile/profile.js
@@ -11,6 +11,8 @@ angular.module('myApp.profile', ['ngRoute'])
 
     .controller('profileCtrl', ['$rootScope', '$scope', '$location', 'userService', function($rootScope, $scope, $location, userService) {
 
+        $scope.error = "";
+
         if (!$rootScope.user) {
             $location.path("/login");
         }
@@ -25,7 +27,7 @@ angular.module('myApp.profile', ['ngRoute'])
             console.log("Pw changed");
             console.log(this.newPassword);
             if(this.newPassword !== this.repeatedNewPassword) {
-
+                $scope.error = "Password mismatch";
             }
             else {
                 userService.change_password(this.newPassword).then(
