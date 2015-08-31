1f3620a51fa2b2c69e1b22589fc103e544c82f0d
andrroy <andreas@royrvik.org>
2015-08-31 14:18:33 +0200

    Added public profile

--- app/views/profile/profile.js
+++ app/views/profile/profile.js
@@ -4,55 +4,74 @@ angular.module('myApp.profile', ['ngRoute'])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/profile', {
-                templateUrl: 'views/profile/profile.html',
-                controller: 'profileCtrl'
-            })
             .when('/user/:id', {
                 templateUrl: 'views/profile/profile.html',
                 controller: 'profileCtrl'
             })
+            .when('/profile', {
+                templateUrl: 'views/profile/profile.html',
+                controller: 'myProfileCtrl'
+            })
     }])
 
-    .controller('profileCtrl', ['$rootScope', '$scope', 'userService', function($rootScope, $scope, userService) {
+    .controller('profileCtrl', ['$scope', '$routeParams', 'userService',
+        function($scope, $routeParams, userService) {
 
-        if (!$rootScope.user) {
-            return $rootScope.goTo("/login");
-        }
+            var userId = $routeParams.id;
 
-        $scope.logout = function () {
-            $rootScope.goTo("/logout");
-        };
-
-        $scope.passwordForm = false;
-
-        $scope.togglePasswordForm = function() {
-            $scope.passwordForm = !$scope.passwordForm;
-        };
-
-        $scope.changePassword = function () {
-            if(this.newPassword !== this.repeatedNewPassword) {
-                $scope.error = "Password mismatch";
-            }
-            else {
-                userService.change_password(this.newPassword).then(
-                    function(res) {
-                        $scope.successMessage = "Your password has been updated";
-                        $scope.passwordForm = false;
-                    },
-                    function(err) {
-                        $scope.error = err;
-                    });
-            }
-        };
-
-        $scope.deleteAccount = function () {
-            userService.deleteAccount($rootScope.user.id).then(
+            userService.getUser(userId).then(
                 function(res) {
-                    $scope.logout();
+                    $scope.user = res;
                 },
                 function(err) {
-                    console.log("Something went wrong")
+                    console.log(err);
                 });
-        };
-    }]);
+        }
+    ])
+
+    .controller('myProfileCtrl', ['$rootScope', '$scope', 'userService',
+        function($rootScope, $scope, userService) {
+
+            if(!$rootScope.user) {
+                return $rootScope.goTo("/login");
+            }
+
+            $scope.user = $rootScope.user;
+
+            $scope.logout = function () {
+                $rootScope.goTo("/logout");
+            };
+
+            $scope.passwordForm = false;
+
+            $scope.togglePasswordForm = function() {
+                $scope.passwordForm = !$scope.passwordForm;
+            };
+
+            $scope.changePassword = function () {
+                if(this.newPassword !== this.repeatedNewPassword) {
+                    $scope.error = "Password mismatch";
+                }
+                else {
+                    userService.change_password(this.newPassword).then(
+                        function(res) {
+                            $scope.successMessage = "Your password has been updated";
+                            $scope.passwordForm = false;
+                        },
+                        function(err) {
+                            $scope.error = err;
+                        });
+                }
+            };
+
+            $scope.deleteAccount = function () {
+                userService.deleteAccount($rootScope.user.id).then(
+                    function(res) {
+                        $scope.logout();
+                    },
+                    function(err) {
+                        console.log("Something went wrong")
+                    });
+            };
+        }
+    ]);
